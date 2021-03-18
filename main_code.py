5d9b23d5eae96ed3f8bd36954408f33d75e7f72f
aene1 <aene@luc.edu>
2021-03-18 15:43:20 -0500

    added main_code.py

--- main_code.py
+++ main_code.py
@@ -2,22 +2,24 @@ import requests
 
 string_api_url = "https://string-db.org/api"
 output_format = "tsv-no-header"
-method = "get_string_ids"
+method = "interaction_partners"
+
+with open('receptorList.txt','r') as f:
+    s = f.read().strip()
+
+
 
 params = {
-
-    "identifiers" : "\r".join(["p53", "BRCA1", "cdk2", "Q99835"]), # your protein list
-    "species" : 9606, # species NCBI identifier
-    "limit" : 1, # only one (best) identifier per input protein
-    "echo_query" : 1, # see your input identifiers in the output
-    "caller_identity" : "www.awesome_app.org" # your app name
+    "identifiers" : "\r".join(["CCR3"]), # your protein list
+    "species" : 9606, # species NCBI identifier  #homo sapiens
+    "echo_query" : 1, # see your input identifiers in the output  #???
+    "limit" : 100
 
 }
 
 ##
 ## Construct URL
 ##
-
 
 request_url = "/".join([string_api_url, output_format, method])
 
@@ -31,7 +33,11 @@ results = requests.post(request_url, data=params)
 ## Read and parse the results
 ##
 
+
 for line in results.text.strip().split("\n"):
     l = line.split("\t")
-    input_identifier, string_identifier = l[0], l[2]
-    print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
+    print(len(l))
+    # print(l)
+    # input_identifier, string_identifier = l[0], l[2]
+    # print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
+
