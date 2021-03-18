fb26b1a237b1e696216d41f948f74baac427b9e3
aene1 <aene@luc.edu>
2021-03-18 16:25:47 -0500

    added main_code.py

--- main_code.py
+++ main_code.py
@@ -2,18 +2,18 @@ import requests
 
 string_api_url = "https://string-db.org/api"
 output_format = "tsv-no-header"
-method = "interaction_partners"
+method = "enrichment" # if used that we get the functional annotation
+#method = "network" # !!!! we get the network !!!!!!
 
 with open('receptorList.txt','r') as f:
     s = f.read().strip()
-
 
 
 params = {
     "identifiers" : "\r".join(["CCR3"]), # your protein list
     "species" : 9606, # species NCBI identifier  #homo sapiens
     "echo_query" : 1, # see your input identifiers in the output  #???
-    "limit" : 100
+    "limit" : 10
 
 }
 
@@ -36,8 +36,7 @@ results = requests.post(request_url, data=params)
 
 for line in results.text.strip().split("\n"):
     l = line.split("\t")
-    print(len(l))
-    # print(l)
+    print(l)
     # input_identifier, string_identifier = l[0], l[2]
     # print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
 
