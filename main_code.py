0ec9d892dd52c2cb3e9be403dbfae3a0594d4c09
Abdullah Mazher <36522429+amazher@users.noreply.github.com>
2021-04-14 16:37:01 -0500

    implemented dictionary to hold data/nodes from STRING

--- main_code.py
+++ main_code.py
@@ -2,7 +2,7 @@ import requests
 
 string_api_url = "https://string-db.org/api"
 output_format = "tsv"
-method = "interaction_partners"
+method = "network"
 #method can be changed based on what we are searching for
 #options: interaction_partners, network, functional_annotation, enrichment
 
@@ -25,6 +25,8 @@ request_url = "/".join([string_api_url, output_format, method])
 results = requests.post(request_url, data=params)
 
 # Read and parse the results (write to outfile)
+# also, read the nodes into a dictionary similar to sif_dict: key=node, value=list of connections
+string_dict = {}
 prev_line = ''
 for line in results.text.strip().split("\n"):
     if line != prev_line:
@@ -32,8 +34,15 @@ for line in results.text.strip().split("\n"):
         outfile.write(str(l) + '\n')
         # input_identifier, string_identifier = l[0], l[2]
         # print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
+        node1 = l[2]
+        node2 = l[3]
+        if node1 in string_dict.keys(): #if node1 already exists as key, add node2 to corresponding list
+            string_dict[node1].append(node2)
+        else: #if node1 doesn't exist as key, create new entry and a list containing node2 as the value
+            string_dict[node1] = [node2]
     prev_line = line
 outfile.close()
+string_dict.pop('preferredName_A') #remove the header from the dictionary
 
 # read in networkOverview.sif as a dictionary
 ## key=node1, value=list of connections to node1
@@ -47,4 +56,3 @@ for line in sif_file: #for each line in file, extract the nodes
         sif_dict[node1].append(node2)
     else: #if node1 doesn't exist as key, create new entry and a list containing node2 as the value
         sif_dict[node1] = [node2]
-
